cdc9bbd2cf2c0b0156dd39f9d773f81304241f97
pauuulaaa <heleinapaula@gmail.com>
2024-10-20 20:24:50 +0800

    Create ObjectActivity.js

---
+++ ObjectActivity.js
@@ -0,0 +1,13 @@
+let car = {
+    type: "Subaru",
+    model: "Crosstrek",
+    color: "white"
+};
+
+console.log(typeof car);
+
+car.type = "Bmw";
+console.log(car);
+
+car.wheels = 4;
+console.log(car);
